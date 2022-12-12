cef1a8f040cd797d5934069c6df0bcd624f91191
tama <pratama.sa@meteor.id>
2022-12-12 10:23:28 +0700

    layouting news item

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -1,17 +1,58 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Button} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import NewsItem from './news_item';
 
 const News = () => {
   const navigation = useNavigation();
   return (
-    <>
-      <Button
-        title="go to detail"
-        onPress={() => navigation.navigate('NewsDetail')}
-      />
-    </>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Text style={styles.title}>Kabar Terbaru</Text>
+        <View style={styles.ContainerNewsItem}>
+          <NewsItem />
+        </View>
+        <View style={styles.ContainerNewsItem}>
+          <NewsItem />
+        </View>
+        <View style={styles.ContainerNewsItem}>
+          <NewsItem />
+        </View>
+        <View style={styles.ContainerNewsItem}>
+          <NewsItem />
+        </View>
+        <View style={styles.ContainerNewsItem}>
+          <NewsItem />
+        </View>
+      </ScrollView>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+  },
+  ContainerNewsItem: {
+    marginBottom: 10,
+  },
+  title: {
+    fontSize: 18,
+    color: '#124492',
+    fontWeight: '600',
+    marginBottom: 12,
+    marginTop: 16,
+    alignSelf: 'flex-start',
+  },
+  containerFloatingFilter: {
+    position: 'absolute',
+    bottom: 20,
+    alignSelf: 'center',
+    left: 16,
+    right: 16,
+  },
+});
 
 export default News;
--- src/components/news_item.jsx
+++ src/components/news_item.jsx
@@ -1,10 +1,76 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {Colors} from '../utils/colors';
+
+const dummyImageUrl =
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtklGsvSriwElsw4yVPh4slVJ-meK2rMjotg&usqp=CAU';
 
 const NewsItem = () => (
-  <View>
-    <Text>NewsItem</Text>
+  <View style={styles.container}>
+    <Image
+      source={{uri: dummyImageUrl}}
+      style={styles.header}
+      resizeMode="cover"
+    />
+    <View style={styles.body}>
+      <Text style={styles.title}>
+        Baruno: Alat Kualitas Air Pintar Handal dan Praktis
+      </Text>
+      <Text style={styles.subtitle} numberOfLines={2} ellipsizeMode="tail">
+        Tahun 2019 telah berlalu, kini tahun 2020 telah memasuki minggu ketiga,
+        memasuki tahun lorempisum dolor sit amet zzzzzzzz
+      </Text>
+      <View style={styles.footer}>
+        <Text style={styles.date}>30 April 2020</Text>
+        <Text>share icon</Text>
+      </View>
+    </View>
   </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 0.5,
+
+    elevation: 0.5,
+  },
+  header: {
+    height: 160,
+    borderTopRightRadius: 5,
+    borderTopLeftRadius: 5,
+  },
+  body: {
+    padding: 14,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    color: Colors.default,
+    fontSize: 20,
+    fontWeight: '600',
+    marginBottom: 9,
+  },
+  subtitle: {
+    color: 'grey',
+    fontSize: 15,
+    marginBottom: 12,
+  },
+  date: {
+    color: 'gray',
+  },
+});
 
 export default NewsItem;
