b2a27299e0e4352a93a1da097c10350b8fe8ebec
tama <pratama.sa@meteor.id>
2022-12-13 13:37:16 +0700

    handle statusbar

--- App.jsx
+++ App.jsx
@@ -11,64 +11,68 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {StatusBar} from 'react-native';
 import ShareButton from './src/components/share_button';
 import DiseasesDetailScreen from './src/screens/disease_detail_screen';
 import HomeScreen from './src/screens/home_screen';
 import MediaScreen from './src/screens/media_screen';
 import NewsDetailScreen from './src/screens/news_detail_screen';
 import PriceDetailScreen from './src/screens/price_detail_screen';
-import {RootStackParamList} from './src/types';
+
 import {Colors} from './src/utils/colors';
 
 const RootStack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootStack.Navigator>
-        <RootStack.Group
-          screenOptions={{
-            headerStyle: {backgroundColor: Colors.primary},
-            headerTitleStyle: {
-              color: 'white',
-            },
-            headerTitleAlign: 'left',
-            headerTintColor: 'white',
-          }}>
-          <RootStack.Screen name="Home" component={HomeScreen} />
-          <RootStack.Screen
-            name="Media"
-            component={MediaScreen}
-            options={{title: 'Jala Media'}}
-          />
+    <>
+      <StatusBar animated={true} backgroundColor={'#003B74'} />
+      <NavigationContainer>
+        <RootStack.Navigator>
           <RootStack.Group
-            screenOptions={props => ({
-              headerRight: () => {
-                const {shareUrl} = props.route.params;
-                return <ShareButton content={shareUrl} />;
+            screenOptions={{
+              headerStyle: {backgroundColor: Colors.primary},
+              headerTitleStyle: {
+                color: 'white',
               },
-            })}>
+              headerTitleAlign: 'left',
+              headerTintColor: 'white',
+            }}>
+            <RootStack.Screen name="Home" component={HomeScreen} />
             <RootStack.Screen
-              name="PriceDetail"
-              component={PriceDetailScreen}
-              options={{
-                title: 'Harga Udang',
-              }}
+              name="Media"
+              component={MediaScreen}
+              options={{title: 'Jala Media'}}
             />
-            <RootStack.Screen
-              name="NewsDetail"
-              component={NewsDetailScreen}
-              options={{title: 'Kabar Udang'}}
-            />
-            <RootStack.Screen
-              name="DiseasesDetail"
-              component={DiseasesDetailScreen}
-              options={{title: 'Info Penyakit'}}
-            />
+            <RootStack.Group
+              screenOptions={props => ({
+                headerRight: () => {
+                  const {shareUrl} = props.route.params;
+                  return <ShareButton content={shareUrl} />;
+                },
+              })}>
+              <RootStack.Screen
+                name="PriceDetail"
+                component={PriceDetailScreen}
+                options={{
+                  title: 'Harga Udang',
+                }}
+              />
+              <RootStack.Screen
+                name="NewsDetail"
+                component={NewsDetailScreen}
+                options={{title: 'Kabar Udang'}}
+              />
+              <RootStack.Screen
+                name="DiseasesDetail"
+                component={DiseasesDetailScreen}
+                options={{title: 'Info Penyakit'}}
+              />
+            </RootStack.Group>
           </RootStack.Group>
-        </RootStack.Group>
-      </RootStack.Navigator>
-    </NavigationContainer>
+        </RootStack.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
 
